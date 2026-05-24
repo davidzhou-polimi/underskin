ac388f819b62286eaf726df2e60ffe398dfae3ad
dingfang818 <fang.ding@mail.polimi.it>
2026-05-24 12:02:52 +0200

    aggiunta del testo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,52 +1,29 @@
 <script>
-	import HeroSection from '$lib/components/sections/HeroSection.svelte';
-	import ScrollBlob from '$lib/components/ui/ScrollBlob.svelte';
-	import GradientBackgroundBlue from '$lib/components/ui/GradientBackgroundBlue.svelte';
-	import Grainient from '$lib/components/ui/Grainient.svelte';
-	import GradientTrailBlue from '$lib/components/ui/GradientTrailBlue.svelte';
-	import GradientBackgroundBlue2 from '$lib/components/ui/GradientBackgroundBlue2.svelte';
+	import GradientBackground from '$lib/components/ui/GradientBackground.svelte';
 </script>
 
-<main class="stacked">
-	<HeroSection />
-	<ScrollBlob />
-	<GradientBackgroundBlue />
-	<div style="width: 100%; height: 100vh; position: relative;">
-		<Grainient
-			color1="#ffffff"
-			color2="#6A96DF"
-			color3="#ffffff"
-			timeSpeed={1}
-			colorBalance={0.1}
-			warpStrength={0}
-			warpFrequency={5.1}
-			warpSpeed={2}
-			warpAmplitude={29}
-			blendAngle={0}
-			blendSoftness={0.05}
-			rotationAmount={500}
-			noiseScale={2}
-			grainAmount={0.1}
-			grainScale={2}
-			grainAnimated={false}
-			contrast={1}
-			gamma={1}
-			saturation={1}
-			centerX={0}
-			centerY={0}
-			zoom={0.9}
-		/>
-	</div>
-	<GradientTrailBlue />
-	<GradientBackgroundBlue2 />
+<main>
+	<GradientBackground />
+	<h1 class="hero-title">Milano-Cortina 2026</h1>
 </main>
 
 <style>
-	.stacked {
+	main {
+		position: relative;
+		min-height: 100vh;
 		display: flex;
-		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.hero-title {
+		position: absolute;
+		color: var(--content-primary);
+		text-align: center;
+		font-family: 'Rethink Sans', sans-serif;
+		font-size: var(--text-xl:  3.5rem);
+		font-weight: 700;
+		line-height: 60px;
 		margin: 0;
-		padding: 0;
-		gap: 0;
 	}
 </style>
