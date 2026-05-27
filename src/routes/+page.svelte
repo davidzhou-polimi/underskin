dc712c24fe0651ce408e6733dba6f34dd0d8a112
Lucrezia Vallar <lucrezia.vallar@mail.polimi.it>
2026-05-27 09:15:18 +0200

    sezione3: testo con definizioni

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,12 +2,14 @@
 	// Importa qui in sequenza le macro-sezioni dalla cartella components/sections/
 	import HeroSection from '$lib/components/sections/HeroSection.svelte';
 	import IntrusiveThoughts from '$lib/components/sections/IntrusiveThoughts.svelte';
+	import FavoritotextSection from '$lib/components/sections/FavoritotextSection.svelte';
 </script>
 
 <main>
 	<!-- Impila qui le sezioni (l'aggiornamento dello store è gestito internamente tramite use:trackSection) -->
 	<HeroSection />
 	<IntrusiveThoughts />
+	<FavoritotextSection />
 </main>
 
 
